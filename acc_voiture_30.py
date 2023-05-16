cc3509690ad98cc47a07f1aa651e9f682d526aa5
Paul Flammarion <paul@flammarion.eu>
2023-05-16 13:38:34 +0200

    add two plots

--- acc_voiture_30.py
+++ acc_voiture_30.py
@@ -3,15 +3,13 @@ from statistics import mean
 import matplotlib.pyplot as plt
 import pandas as pd
 
-g = 9.81
-point_equilibre_acceleration = 0.2
-point_equilibre_deceleration = 0.2
-
-acceleration_seuil_max = 0.6
-acceleration_seuil_min = -0.05
-
-filename = "30.csv"
+filenames = ["30", "50", "80"]
 colname = "gFy"
+
+plt.figure().clear()
+plt.close()
+plt.cla()
+plt.clf()
 
 def filter_data(filename):
     df = pd.read_csv(filename, delimiter=';')
@@ -21,71 +19,40 @@ def filter_data(filename):
     y = df[colname].tolist()
     return x, y
 
-def calculate_acceleration(x, y):
 
-    min_index = y.index(min(y))
-    max_index = y.index(max(y))
+def calculate_instantaneous_velocity(acceleration, time_interval, initial_velocity):
+    velocity = [initial_velocity]
+    velocity.insert(0,0)
+    for i in range(1, len(acceleration) - 1):
+        delta_t = time_interval[i+1] - time_interval[i]
+        delta_v = 0.5 * (acceleration[i+1] + acceleration[i]) * delta_t
+        v = velocity[i-1] + delta_v
+        velocity.append(v)
+    return velocity
 
+def calculate_distance(velocity, time_interval):
+    distance = [0]
+    for i in range(1, len(velocity)):
+        delta_t = time_interval[i] - time_interval[i-1]
+        delta_d = 0.5 * (velocity[i] + velocity[i-1]) * delta_t
+        d = distance[i-1] + delta_d
+        distance.append(d)
+    return distance
 
-    for i in range(max_index, -1, -1):
-        if y[i] < point_equilibre_acceleration:
-            debut_acc = i
-            break
+for filename in filenames:
+    x, y = filter_data(filename + ".csv")
+    velocity = calculate_instantaneous_velocity(y, x, 0)
+    total_distance = sum([v * (x[i+1] - x[i]) for i, v in enumerate(velocity[:-1])])
 
-    for i in range(max_index+1, len(y)):
-        if y[i] < point_equilibre_acceleration - 0.2:
-            fin_acc = i - 1
-            break
+    plt.plot(x, y, 'ro', label='Acceleration (en m/s^2)',  markersize=2)
+    plt.plot(x, velocity, 'bo', label='Vitesse (en m/s)',  markersize=2)
 
-    for i in range(min_index, -1, -1):
-        if y[i] > point_equilibre_deceleration - 0.2:
-            debut_dece = i + 1
-            break
+    plt.title("Profil de l'accélération et de la vitesse d'une voiture de 0 à " + filename + " km/h")
+    plt.ylabel('y')
+    plt.xlabel('Temps (s)')
 
-    for i in range(min_index+1, len(y)):
-        if y[i] > point_equilibre_deceleration:
-            fin_dece = i
-            break
+    plt.legend()
+    plt.text(0.02, 0.8, "Distance totale parcourue: {:.2f} m".format(total_distance), transform=plt.gca().transAxes)
 
+    plt.show()
 
-    return debut_acc, fin_acc, debut_dece, fin_dece
-
-x, y = filter_data(filename)
-
-plt.plot(x, y, 'ro')
-plt.title('Profil de l''accélération lors de la descente de l\'idir ')
-plt.xlabel('Temps (s)')
-plt.ylabel('Accélération (en m/s^-2)')
-plt.show()
-
-debut_acc, fin_acc, debut_dece, fin_dece = calculate_acceleration(x, y)
-
-print(debut_acc, fin_acc, debut_dece, fin_dece)
-
-mean_acc = (mean(y[debut_acc:fin_dece]) * g)-g
-mean_dece = (mean(y[debut_dece:fin_dece]) * g)-g
-
-delta_acc = x[debut_acc] - x[fin_dece]
-delta_dece = x[debut_dece] - x[fin_dece]
-
-print("L'acceletration est positive de {:.2f} s à {:.2f} s avec une accelération moyenne de {:.2f} m/s^2".format(x[debut_acc], x[fin_dece], mean_acc))
-print("L'acceletration est negative de {:.2f} s à {:.2f} s avec une deceleration moyenne de {:.2f} m/s^2".format(x[debut_dece], x[fin_dece], mean_dece))
-
-print("")
-
-vitesse_acc = mean_acc * delta_acc
-distance_acc = abs(mean_acc * delta_acc**2)
-
-vitesse_dece = mean_dece * delta_dece
-distance_dece = abs(mean_dece * delta_dece**2)
-
-print("On en déduit une acceleration pendant {:.2f} m à une vitesse de {:.2f} m/s".format(distance_acc, abs(vitesse_acc)))
-print("On en déduit une deceleration pendant {:.2f} m à une vitesse de {:.2f} m/s".format(abs(distance_dece), vitesse_dece))
-
-print("")
-
-
-distance_tot = distance_acc + abs(distance_dece)
-temps_tot = x[fin_dece] - x[debut_acc]
-
-print("Ce qui fait un total de {:.2f} m en {:.2f} s".format(distance_tot, temps_tot))
