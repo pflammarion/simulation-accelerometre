c6b7f47d3b989ddba2b572b99c28ca2f37c8ae06
Paul Flammarion <paul@flammarion.eu>
2023-05-27 15:55:01 +0200

    update to new analyse

--- acc_voiture.py
+++ acc_voiture.py
@@ -1,9 +1,9 @@
-from statistics import mean
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
-filenames = ["30", "50", "80"]
+filenames = ["30", "50", "80", "bruit"]
 colname = "gFy"
 
 plt.figure().clear()
@@ -13,24 +13,20 @@ plt.clf()
 
 
 def filter_data(filename):
-    df = pd.read_csv(filename, delimiter=';')
-    df[colname] = df[colname].str.replace(',', '.').astype(float)
+    if filename != "./data/bruit.csv" :
+        df = pd.read_csv(filename, delimiter=';')
+        df[colname] = df[colname].str.replace(',', '.').astype(float)
+    else :
+        df = pd.read_csv(filename, delimiter=',')
     df = df.drop_duplicates(subset='time', keep='last')
-    x = df['time'].tolist()
-    y = df[colname].tolist()
-    y = [acc - y[0] for acc in y]
+    x = df['time']
+    y = df[colname]
+    y = y - y[0]
     return x, y
 
 
-def calculate_instantaneous_velocity(acceleration, time_interval, initial_velocity):
-    velocity = [initial_velocity]
-    velocity.insert(0, 0)
-    for i in range(1, len(acceleration) - 1):
-        delta_t = time_interval[i + 1] - time_interval[i]
-        delta_v = 0.5 * (acceleration[i + 1] + acceleration[i]) * delta_t
-        v = velocity[i - 1] + delta_v
-        velocity.append(v)
-    return velocity
+def calculate_instantaneous_velocity(acceleration, time_interval):
+    return (acceleration[:-1] * np.diff(time_interval)).cumsum()
 
 
 def calculate_distance(velocity, time_interval):
@@ -42,16 +38,25 @@ def calculate_distance(velocity, time_interval):
         distance.append(d)
     return distance
 
+plt.clf()
 
 for filename in filenames:
-    x, y = filter_data("./data/" + filename + ".csv")
-    velocity = calculate_instantaneous_velocity(y, x, 0)
+    x, y = filter_data(f"./data/{filename}.csv")
+    y = y * 9.81
+    velocity = calculate_instantaneous_velocity(y, x)
 
-    plt.plot(x, y, 'ro', label='Acceleration (en m/s^2)', markersize=2)
-    plt.plot(x, velocity, 'bo', label='Vitesse (en m/s)', markersize=2)
+    ax = plt.gca()
+    ax2 = ax.twinx()
+
+
+    ax.plot(x[:-1], y[:-1], 'ro', label='Acceleration (en m/s^2)', markersize=2)
+    ax.set_ylabel('Acceleration (en m/s^2)')
+    ax2.plot(x[:-1], velocity * 3.6, 'bo', label='Vitesse (en m/s)', markersize=2)
+    ax2.set_ylabel('Vitesse (en km/h)')
+    ax.grid()
 
     plt.title("Profil de l'accélération et de la vitesse d'une voiture de 0 à " + filename + " km/h")
-    plt.xlabel('Temps (s)')
+    ax.set_xlabel('Temps (s)')
 
     plt.legend()
     plt.show()
