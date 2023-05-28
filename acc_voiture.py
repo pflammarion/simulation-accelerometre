fe8daec73f1bed1b7d6c6df8c13e020a44538b7f
Paul Flammarion <paul@flammarion.eu>
2023-05-28 20:29:16 +0200

    update code to numpy analyse

--- acc_voiture.py
+++ acc_voiture.py
@@ -3,60 +3,72 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-filenames = ["30", "50", "80", "bruit"]
-colname = "gFy"
+filenames = ["30", "50", "80"]
 
 plt.figure().clear()
 plt.close()
 plt.cla()
 plt.clf()
 
-
 def filter_data(filename):
-    if filename != "./data/bruit.csv" :
-        df = pd.read_csv(filename, delimiter=';')
-        df[colname] = df[colname].str.replace(',', '.').astype(float)
-    else :
-        df = pd.read_csv(filename, delimiter=',')
+
+    df = pd.read_csv(filename, delimiter=';')
+    if (filename == "./data/bruit.csv") :
+        df["time"] = df["time"].str.replace(',', '.').astype(float)
+
+    df["gFy"] = df["gFy"].str.replace(',', '.').astype(float)
+    y = df["gFy"]
     df = df.drop_duplicates(subset='time', keep='last')
     x = df['time']
-    y = df[colname]
     y = y - y[0]
+
     return x, y
 
 
 def calculate_instantaneous_velocity(acceleration, time_interval):
     return (acceleration[:-1] * np.diff(time_interval)).cumsum()
 
+def calculate_noise(x, y, time):
+    index = np.where(x > time)[0][0]
+    return (y[index]/time) * x[:-1]
 
-def calculate_distance(velocity, time_interval):
-    distance = [0]
-    for i in range(1, len(velocity)):
-        delta_t = time_interval[i] - time_interval[i - 1]
-        delta_d = 0.5 * (velocity[i] + velocity[i - 1]) * delta_t
-        d = distance[i - 1] + delta_d
-        distance.append(d)
-    return distance
+def filter_velocity(velocity_noise, noise):
+    if len(velocity_noise) < len(noise):
+        return velocity_noise - noise[:len(velocity_noise)]
+    else:
+        noise_extended = np.repeat(noise.iloc[-1], len(velocity_noise))
+        noise_extended = noise_extended[:len(velocity_noise)]
+        return velocity_noise - noise_extended
 
-plt.clf()
+
+# to remove the constant noise
+x, y = filter_data("./data/bruit.csv")
+noise = calculate_instantaneous_velocity(y, x)
 
 for filename in filenames:
     x, y = filter_data(f"./data/{filename}.csv")
     y = y * 9.81
-    velocity = calculate_instantaneous_velocity(y, x)
+    velocity_noise = calculate_instantaneous_velocity(y, x)
 
-    ax = plt.gca()
+    velocity = filter_velocity(velocity_noise, noise)
+
+
+    fig, ax = plt.subplots()
     ax2 = ax.twinx()
 
 
-    ax.plot(x[:-1], y[:-1], 'ro', label='Acceleration (en m/s^2)', markersize=2)
-    ax.set_ylabel('Acceleration (en m/s^2)')
-    ax2.plot(x[:-1], velocity * 3.6, 'bo', label='Vitesse (en m/s)', markersize=2)
-    ax2.set_ylabel('Vitesse (en km/h)')
-    ax.grid()
+    ax2.plot(x[:-1], y[:-1], 'green', markersize=1, zorder=0)
+    ax2.set_ylabel('Acceleration en G')
+
+    ax.plot(x[:-1], velocity_noise * 3.6, 'bo', label='Vitesse avec bruit (en m/s)', markersize=2, zorder=10)
+    ax.plot(x[:-1], velocity * 3.6, 'ro', label='Vitesse sans bruit (en m/s)', markersize=2, zorder=5)
+    ax.set_ylabel('Vitesse (en km/h)')
 
     plt.title("Profil de l'accélération et de la vitesse d'une voiture de 0 à " + filename + " km/h")
     ax.set_xlabel('Temps (s)')
 
-    plt.legend()
+    lines, labels = ax.get_legend_handles_labels()
+    ax.legend(lines, labels, loc='upper left')
+
+    plt.grid(True)
     plt.show()
